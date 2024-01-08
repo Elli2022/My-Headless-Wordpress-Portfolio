f5308982660852c9dd77a92584d7ce1857a0ab75
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-01-08 14:44:19 +0100

    styling

--- src/app/projects/[slugs]/page.tsx
+++ src/app/projects/[slugs]/page.tsx
@@ -1,6 +1,5 @@
 // src/pages/projects/[slugs]/page.tsx
 
-
 import React from "react";
 import getPages from "@/pages/queries/getPages";
 import Navigation from "../../components/Navigation";
@@ -8,7 +7,6 @@ import WP from "@/pages/api/wp";
 import getPost from "../../../pages/queries/getPost";
 import ProjectPost from "../../components/ProjectPost";
 import Footer from "@/app/components/Footer";
-
 
 interface Post {
   title: string;
@@ -73,10 +71,11 @@ let globalPostData: {
 const ProjectPage = async ({ params }: { params: { slugs: string } }) => {
   console.log("Received slug:", params.slugs); // Logga mottagen slug
   let globalPostData = null;
-  let additionalPostInfo = null;  // Definiera additionalPostInfo här
+  let additionalPostInfo = null; // Definiera additionalPostInfo här
 
   // Hämta data från WP
-  const resPost = await WP(`
+  const resPost = await WP(
+    `
     query GetPostBySlug($slug: String!) {
       postBy(slug: $slug) {
         id
@@ -107,11 +106,11 @@ const ProjectPage = async ({ params }: { params: { slugs: string } }) => {
   console.log("Additional Data Response:", additionalData); // Logga ytterligare data
 
   if (additionalData && additionalData.data) {
-    additionalPostInfo = additionalData.data.post;  // Tilldela data till additionalPostInfo
+    additionalPostInfo = additionalData.data.post; // Tilldela data till additionalPostInfo
     console.log("Additional Post Info:", additionalPostInfo); // Logga ytterligare post info
   }
 
-      console.log(additionalData.data.post.PostInfo.branding);
+  console.log(additionalData.data.post.PostInfo.branding);
 
   const navlinks = await getPages();
   const navHits = navlinks.edges.map((edge: any) => edge.node);
@@ -125,155 +124,184 @@ const ProjectPage = async ({ params }: { params: { slugs: string } }) => {
     return <div>Loading...</div>;
   }
 
-
   // Kontrollera om 'blocks' finns och är en array innan du försöker mappa över den
-const keyFindingsBlock = additionalPostInfo?.PostInfo?.blocks?.find(
-  (block: { fieldGroupName: string; }) => block.fieldGroupName === 'Post_Postinfo_Blocks_Keyfindings'
-);
+  const keyFindingsBlock = additionalPostInfo?.PostInfo?.blocks?.find(
+    (block: { fieldGroupName: string }) =>
+      block.fieldGroupName === "Post_Postinfo_Blocks_Keyfindings"
+  );
 
-// Locate the picture block in the additionalPostInfo
-const pictureBlock = additionalPostInfo?.PostInfo?.blocks?.find(
-  (block: { fieldGroupName: string; }) => block.fieldGroupName === 'Post_Postinfo_Blocks_Picture'
-);
+  // Locate the picture block in the additionalPostInfo
+  const pictureBlock = additionalPostInfo?.PostInfo?.blocks?.find(
+    (block: { fieldGroupName: string }) =>
+      block.fieldGroupName === "Post_Postinfo_Blocks_Picture"
+  );
 
-
-
-//Destructering- värden för att rendera komponenter
+  //Destructering- värden för att rendera komponenter
   const {
     branding,
     text = "Default text if null",
     nextprojecttext = "Default next project text if null",
     replaceurl = "#", // Standardvärde om inget finns
     replacetext = "Default replace text if null", // Standardvärde om inget finns
-    liveworkbuttonURL="#",
+    liveworkbuttonURL = "#",
     liveworkbuttontext = "Default replace text if null",
-    tosatisfyourgoaltext = "Default text if null", 
+    tosatisfyourgoaltext = "Default text if null",
     blocks = [],
-  } = additionalData?.data?.post?.PostInfo || {}; 
+  } = additionalData?.data?.post?.PostInfo || {};
 
+  const renderImages = (blocks: any[]) => {
+    // Extrahera bilder från blocks
+    const images = blocks
+      .filter(
+        (block: { fieldGroupName: string }) =>
+          block.fieldGroupName === "Post_Postinfo_Blocks_Images"
+      )
+      .flatMap((block: { projectimages: any }) => block.projectimages);
 
-const renderImages = (blocks: any[]) => {
-  // Extrahera bilder från blocks
-  const images = blocks
-    .filter((block: { fieldGroupName: string; }) => block.fieldGroupName === 'Post_Postinfo_Blocks_Images')
-    .flatMap((block: { projectimages: any; }) => block.projectimages);
-
-
-  return (
-    <div className="grid grid-cols-2 gap-4">
-      {images.map((image: { mediaItemUrl: string | undefined; }, index: number) => (
-        <img 
-          key={index}
-          src={image.mediaItemUrl} 
-          alt={`Image ${index}`}
-          className={`w-full object-cover ${index === 0 ? 'col-span-2' : 'col-span-1'}`}
-        />
-      ))}
-    </div>
-  );
-};
-;
-
+    return (
+      <div className="grid grid-cols-2 gap-4">
+        {images.map(
+          (image: { mediaItemUrl: string | undefined }, index: number) => (
+            <img
+              key={index}
+              src={image.mediaItemUrl}
+              alt={`Image ${index}`}
+              className={`w-full object-cover ${
+                index === 0 ? "col-span-2" : "col-span-1"
+              }`}
+            />
+          )
+        )}
+      </div>
+    );
+  };
   // När data är hämtad, renderas sidan
   return (
-    
-    <><div className="min-h-screen bg-gradient-to-b from-[#d6dbdc] to-white text-black p-24">
-   
-      <Navigation 
-        portfolioLink={mainLinks.portfolio}
-        aboutLink={mainLinks.about}
-        contactLink={mainLinks.contact}  />
+    <>
+      <div className="min-h-screen bg-gradient-to-b from-[#d6dbdc] to-white text-black p-24">
+        <Navigation
+          portfolioLink={mainLinks.portfolio}
+          aboutLink={mainLinks.about}
+          contactLink={mainLinks.contact}
+        />
 
-<ProjectPost postData={globalPostData} />
-      <div >
-      <h2 className="text-gray-500 uppercase tracking-wide text-xs">BRANDING</h2>
-        <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mt-2">{additionalData.data.post.PostInfo.projectintrotext}</h1>
-        <p className="text-left text-base mt-2">{tosatisfyourgoaltext}</p>
-
-        <div className="flex flex-row items-center mt-4 space-x-4">
-  {/* Client heading och client */}
-  <div className="flex flex-col space-y-1">
-    <h3 className="text-gray-500 uppercase tracking-wide text-xs">
-      {additionalData.data.post.PostInfo.clientheading}
-    </h3>
-    <p>{additionalData.data.post.PostInfo.client}</p>
-  </div>
-
-  {/* Time och date */}
-  <div className="flex flex-col space-y-1 items-center">
-    <p className="text-gray-500 uppercase tracking-wide text-xs">
-      {additionalData.data.post.PostInfo.time}
-    </p>
-    <p>{additionalData.data.post.PostInfo.date}</p>
-  </div>
-
-  {/* Live work knappen */}
-  <div>
-    <a href={liveworkbuttontext} className="btn inline-block" style={{ padding: '10px 40px 10px 25px', position: 'relative' }}>
-      {liveworkbuttontext}
-      <span style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}>&rarr;</span>
-    </a>
-  </div>
-</div>
-
-     
-  <div className="grid-cols-2 mb-20 gap-2 lg:gap-9">
-  {renderImages(additionalPostInfo?.PostInfo?.blocks || [])}
-</div>
-
-
-{keyFindingsBlock && (
-  <div className="grid grid-cols-2 grid-rows-2 gap-4">
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-lg font-semibold">{keyFindingsBlock.basics}</h2>
-      <p>{keyFindingsBlock.basicstext}</p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-lg font-semibold">{keyFindingsBlock.goals}</h2>
-      <p>{keyFindingsBlock.goalstext}</p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-lg font-semibold">{keyFindingsBlock.problems}</h2>
-      <p>{keyFindingsBlock.problemstext}</p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-lg font-semibold">{keyFindingsBlock.solutions}</h2>
-      <p>{keyFindingsBlock.solutionstext}</p>
-    </div>
-  </div>
-)}
-
-      </div>
-      {pictureBlock && (
+        <ProjectPost postData={globalPostData} />
         <div>
-          <img src={pictureBlock.picture.mediaItemUrl} alt="Block Image" />
+          <h2 className="text-gray-500 uppercase tracking-wide text-xs mt-20 mb-4">
+            BRANDING
+          </h2>
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mt-5 mb-4">
+            {additionalData.data.post.PostInfo.projectintrotext}
+          </h1>
+          <p className="text-left text-base mt-7 mb-4">
+            {tosatisfyourgoaltext}
+          </p>
+
+          <div className="flex flex-row items-center space-x-4">
+            {/* Client heading och client */}
+            <div className="flex flex-col space-y-1 mb-3">
+              <h3 className="text-gray-500 uppercase tracking-wide text-xs">
+                {additionalData.data.post.PostInfo.clientheading}
+              </h3>
+              <p>{additionalData.data.post.PostInfo.client}</p>
+            </div>
+
+            {/* Time och date */}
+            <div className="flex flex-col space-y-1 items-center mb-3">
+              <h3 className="text-gray-500 uppercase tracking-wide text-xs">
+                {additionalData.data.post.PostInfo.time}
+              </h3>
+              <p>{additionalData.data.post.PostInfo.date}</p>
+            </div>
+
+            {/* Live work knappen */}
+            <div className="mb-9">
+              <a
+                href={liveworkbuttontext}
+                className="btn inline-block"
+                style={{ padding: "10px 40px 10px 25px", position: "relative" }}
+              >
+                {liveworkbuttontext}
+                <span
+                  style={{
+                    position: "absolute",
+                    right: "10px",
+                    top: "50%",
+                    transform: "translateY(-50%)",
+                  }}
+                >
+                  &rarr;
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
-      )}
 
-      <div><p>{nextprojecttext}</p></div>
-      <div className="text-center mt-10">
-        <a href={replaceurl} className="btn inline-block my-4" style={{ position: 'relative', padding: '10px 40px 10px 25px' }}>
-          {replacetext}
-          <span style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', fontSize: 'larger' }}>&rarr;</span>
-        </a>
-      </div>
-      <div><h2>{text}</h2></div>
-      <div>
+        {pictureBlock && (
+          <div className="relative h-72 mb-20 bg-dark-kobolt-blue flex justify-center mt-24 md:h-96 ">
+            <div className="relative ">
+              {/* Overlay box */}
+              <div
+                className="absolute inset-x-0 top-1/3 w-full bg-[#034753] shadow-md"
+                style={{ height: "50vh" }}
+              ></div>
+              {/* Image on top of the overlay box */}
+              <img
+                src={pictureBlock.picture.mediaItemUrl}
+                alt="Block Image"
+                className="w-full relative"
+                style={{ zIndex: 0 }}
+              />
+            </div>
+          </div>
+        )}
 
-      </div>
+        <div>
+          <p>{nextprojecttext}</p>
+        </div>
+        <div className="text-center mt-10">
+          <a
+            href={replaceurl}
+            className="btn inline-block my-4"
+            style={{ position: "relative", padding: "10px 40px 10px 25px" }}
+          >
+            {replacetext}
+            <span
+              style={{
+                position: "absolute",
+                right: "10px",
+                top: "50%",
+                transform: "translateY(-50%)",
+                fontSize: "larger",
+              }}
+            >
+              &rarr;
+            </span>
+          </a>
+        </div>
+        <div>
+          <h2>{text}</h2>
+        </div>
 
-      <div>
-          <a href="mailto:contact@folio.design" className="text-blue-500 hover:underline">contact@folio.design</a>
-          <span> | </span>
-          <a href="https://www.linkedin.com/company/folio" className="text-blue-500 hover:underline">LINKEDIN.COM/FOLIO</a>
+        <div className="flex justify-center items-center space-x-2">
+          <a
+            href="mailto:contact@folio.design"
+            className="text-blue-500 hover:underline"
+          >
+            contact@folio.design
+          </a>
+          <span>|</span>
+          <a
+            href="https://www.linkedin.com/company/folio"
+            className="text-blue-500 hover:underline"
+          >
+            LINKEDIN.COM/FOLIO
+          </a>
         </div>
         <Footer />
-    </div>
+      </div>
     </>
-  
   );
-  
 };
-
 
 export default ProjectPage;
