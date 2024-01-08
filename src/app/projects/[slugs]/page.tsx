a77af88562478496985fdb8f2133c823998184ff
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-01-08 15:01:28 +0100

    layout fix

--- src/app/projects/[slugs]/page.tsx
+++ src/app/projects/[slugs]/page.tsx
@@ -193,27 +193,29 @@ const ProjectPage = async ({ params }: { params: { slugs: string } }) => {
           <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold mt-5 mb-4">
             {additionalData.data.post.PostInfo.projectintrotext}
           </h1>
-          <p className="text-left text-base mt-7 mb-6">{tosatisfyourgoaltext}</p>
+          <p className="text-left text-base mt-7 mb-6">
+            {tosatisfyourgoaltext}
+          </p>
 
-          <div className="flex flex-row items-center space-x-4 mb-10">
+          <div className="flex flex-row items-center space-x-14 mb-10">
+            {" "}
+            {/* Ökat space-x-* värde */}
             {/* Client heading och client */}
-            <div className="flex flex-col space-y-2">
+            <div className="flex flex-col space-y-1 items-center mb-4">
               <h3 className="text-gray-500 uppercase tracking-wide text-xs mb-2">
                 {additionalData.data.post.PostInfo.clientheading}
               </h3>
               <p>{additionalData.data.post.PostInfo.client}</p>
             </div>
-
             {/* Time och date */}
-            <div className="flex flex-col space-y-1 items-center">
+            <div className="flex flex-col space-y-1 items-center mb-4">
               <h3 className="text-gray-500 uppercase tracking-wide text-xs mb-2">
                 {additionalData.data.post.PostInfo.time}
               </h3>
               <p>{additionalData.data.post.PostInfo.date}</p>
             </div>
-
             {/* Live work knappen */}
-            <div className="mb-5">
+            <div className="mb-6">
               <a
                 href={liveworkbuttontext}
                 className="btn inline-block"
@@ -239,66 +241,68 @@ const ProjectPage = async ({ params }: { params: { slugs: string } }) => {
           </div>
 
           <h1 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-4xl 4xl:text-4xl font-semibold">
-  {keyFindingsBlock.keyfindingstitle}
-</h1>
+            {keyFindingsBlock.keyfindingstitle}
+          </h1>
 
- 
-{keyFindingsBlock && (
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-xl font-semibold">
-        {keyFindingsBlock.basics}
-      </h2>
-      <p className="text-left text-sm sm:text-base md:text-lg font-light">
-        {keyFindingsBlock.basicstext}
-      </p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
-        {keyFindingsBlock.goals}
-      </h2>
-      <p className="text-left text-sm sm:text-base md:text-lg font-light">
-        {keyFindingsBlock.goalstext}
-      </p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
-        {keyFindingsBlock.problems}
-      </h2>
-      <p className="text-left text-sm sm:text-base md:text-lg font-light">
-        {keyFindingsBlock.problemstext}
-      </p>
-    </div>
-    <div className="flex flex-col p-4 bg-white rounded-lg shadow">
-      <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
-        {keyFindingsBlock.solutions}
-      </h2>
-      <p className="text-left text-sm sm:text-base md:text-lg font-light">
-        {keyFindingsBlock.solutionstext}
-      </p>
-    </div>
-  </div>
-)}
-
+          {keyFindingsBlock && (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              <div className="flex flex-col p-4 bg-white rounded-lg shadow">
+                <h2 className="text-sm sm:text-base md:text-lg lg:text-xl xl:text-2xl 2xl:text-xl font-semibold">
+                  {keyFindingsBlock.basics}
+                </h2>
+                <p className="text-left text-sm sm:text-base md:text-lg font-light">
+                  {keyFindingsBlock.basicstext}
+                </p>
+              </div>
+              <div className="flex flex-col p-4 bg-white rounded-lg shadow">
+                <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
+                  {keyFindingsBlock.goals}
+                </h2>
+                <p className="text-left text-sm sm:text-base md:text-lg font-light">
+                  {keyFindingsBlock.goalstext}
+                </p>
+              </div>
+              <div className="flex flex-col p-4 bg-white rounded-lg shadow">
+                <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
+                  {keyFindingsBlock.problems}
+                </h2>
+                <p className="text-left text-sm sm:text-base md:text-lg font-light">
+                  {keyFindingsBlock.problemstext}
+                </p>
+              </div>
+              <div className="flex flex-col p-4 bg-white rounded-lg shadow">
+                <h2 className="text-sm sm:text-base md:text-lg text-base sm:text-lg md:text-xl font-semibold">
+                  {keyFindingsBlock.solutions}
+                </h2>
+                <p className="text-left text-sm sm:text-base md:text-lg font-light">
+                  {keyFindingsBlock.solutionstext}
+                </p>
+              </div>
+            </div>
+          )}
         </div>
 
-     
         {pictureBlock && (
           <div className="relative h-72 mb-20 bg-dark-kobolt-blue flex justify-center mt-24 md:h-96 ">
-          <div className="relative ">
-            {/* Overlay box */}
-            <div
-              className="absolute inset-x-0 top-1/3 w-full bg-[#034753] shadow-md"
-              style={{ height: "50vh" }}
-            ></div>
-            {/* Image on top of the overlay box */}
-            <img src={pictureBlock.picture.mediaItemUrl} alt="Block Image" className="w-full relative" style={{ zIndex: 0 }} />
-          </div>
+            <div className="relative ">
+              {/* Overlay box */}
+              <div
+                className="absolute inset-x-0 top-1/3 w-full bg-[#034753] shadow-md"
+                style={{ height: "50vh" }}
+              ></div>
+              {/* Image on top of the overlay box */}
+              <img
+                src={pictureBlock.picture.mediaItemUrl}
+                alt="Block Image"
+                className="w-full relative"
+                style={{ zIndex: 0 }}
+              />
+            </div>
           </div>
         )}
 
         <div>
-          <p >{nextprojecttext}</p>
+          <p>{nextprojecttext}</p>
         </div>
         <div className="text-center mt-10">
           <a
