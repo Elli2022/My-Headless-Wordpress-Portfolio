2741f85937eef3d053948fd7cc1321fb03f7b211
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-01-11 13:24:25 +0100

    refactored + more clean code + hasPosts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-// src/app/page.tsx
 import React from "react";
 import Link from "next/link";
 import PaginationControls from "./components/PaginationControls";
@@ -9,238 +8,130 @@ import getHome from "@/pages/queries/getHome";
 import getPages from "@/pages/queries/getPages";
 import getPosts from "@/pages/queries/getPosts";
 
-interface Post {
-  id: string;
-  title: string;
-  content: string;
-  category: string;
-  featuredImage?: {
-    node: {
-      mediaItemUrl: string;
-      slug: string;
-    };
-  };
-  slug?: string;
-  PostInfo?: {
-    subtitle: string;
-  };
-  categories: any;
-}
-
-interface Category {
-  databaseId: number;
-  name: string;
-  id: string;
-}
-
-
 export default async function Home({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  // Om 'page' och 'per_page' är arrays, använd första värdet. Annars, använd värdet direkt.
-  const page = Array.isArray(searchParams["page"])
-    ? searchParams["page"][0]
-    : searchParams["page"] ?? "1";
-  const perPage = Array.isArray(searchParams["per_page"])
-    ? searchParams["per_page"][0]
-    : searchParams["per_page"] ?? "6";
+  // Hantering av sökparametrar för sidnummer och antal poster per sida.
+  const page = Array.isArray(searchParams["page"]) ? searchParams["page"][0] : searchParams["page"] ?? "1";
+  const perPage = Array.isArray(searchParams["per_page"]) ? searchParams["per_page"][0] : searchParams["per_page"] ?? "6";
 
-  // Hantera 'after' på samma sätt
-  const endCursor = Array.isArray(searchParams["after"])
-    ? searchParams["after"][0]
-    : searchParams["after"] ?? "";
-  const beforeCursor = Array.isArray(searchParams["before"])
-    ? searchParams["before"][0]
-    : searchParams["before"] ?? "";
-  // Hämta kategori-ID från searchParams
-  const categoryId = Array.isArray(searchParams["categoryId"])
-    ? searchParams["categoryId"][0]
-    : searchParams["categoryId"];
+  // Hantering av sökparametrar för paginering.
+  const endCursor = Array.isArray(searchParams["after"]) ? searchParams["after"][0] : searchParams["after"] ?? "";
+  const beforeCursor = Array.isArray(searchParams["before"]) ? searchParams["before"][0] : searchParams["before"] ?? "";
 
-  const name = Array.isArray(searchParams["name"])
-    ? searchParams["name"][0]
-    : searchParams["name"] ?? "";
+  // Hantering av kategori-ID från sökparametrar.
+  const categoryId = Array.isArray(searchParams["categoryId"]) ? searchParams["categoryId"][0] : searchParams["categoryId"];
 
+  // Hämtning av inlägg, kategorier och sidinformation från API.
   const { posts, categories, pageInfo } = await getPosts(
-    Number(searchParams["page"]) || 1,
-    Number(searchParams["per_page"]) || 6,
+    Number(page) || 1,
+    Number(perPage) || 6,
     searchParams["after"] as string,
     searchParams["before"] as string,
     categoryId
   );
 
-  // Debugging: Log the posts array
-  // console.log("Posts:", posts);
-
-  // Debugging: Log the slug of each post
-  console.log(
-    "Post slugs:",
-    posts.map((post: any) => post.slug)
-  );
-
-  console.log("Posts from API:", posts);
-  console.log("Categories from API:", categories);
-  // Hämtar data...
+  // Hämtning av ytterligare data för hemsidan och navigationslänkar.
   const data = await getHome("/home");
-  // console.log("Home data:", data);
-
   const navlinks = await getPages();
-  // console.log("Navigation links:", navlinks);
-
-  const postsData = await getPosts();
-  console.log("PostData:", postsData);
-
   const navHits = Object.values(navlinks.edges).map((hit: any) => hit.node);
-  // console.log("Navhits: ", navHits);
 
-  // Identifiera länkar för "Portfolio", "About", och "Contact"
+  // Skapande av huvudlänkar för navigering.
   const mainLinks = {
     portfolio: navHits.find((hit: any) => hit.title === "Portfolio."),
     about: navHits.find((hit: any) => hit.title === "about me."),
     contact: navHits.find((hit: any) => hit.title === "contact."),
   };
 
-  // Debugging: Log the slug of each post
-  console.log(
-    "Post slugs:",
-    posts.map((post: any) => post.slug)
-  );
-
-  console.log("Categories: ", categories);
-
-  // Hämta kategori-ID från searchParams
-  console.log("CategoryId:", categoryId);
-
-  // Filtrera inlägg baserat på kategori
+  // Filtrering av inlägg baserat på vald kategori.
   let filteredPosts = posts;
   if (categoryId) {
-    const categoryMatch = categories.find(
-      (category: { databaseId: any }) => category.databaseId === categoryId
-    );
+    const categoryMatch = categories.find((category: { databaseId: any }) => category.databaseId === categoryId);
     if (categoryMatch) {
-      // Antag att varje post har ett fält 'categoryDatabaseId'
-      filteredPosts = posts.filter(
-        (post: { categoryDatabaseId: any }) =>
-          post.categoryDatabaseId === categoryId
-      );
+      filteredPosts = posts.filter((post: { categoryDatabaseId: any }) => post.categoryDatabaseId === categoryId);
     }
   }
+  const hasPosts = filteredPosts.length > 0;
 
-  console.log(`Filtered Posts for Category ID ${categoryId}:`, filteredPosts);
-  console.log(
-    `Number of Posts matching Category ID ${categoryId}:`,
-    filteredPosts.length
-  );
-
+  // Huvudrenderingslogik.
   return (
     <main className="min-h-screen bg-gradient-to-b from-[#d6dbdc] to-white text-black p-4 md:p-15">
-      {/* Navigationsmenyn */}
+      {/* Navigationsmeny */}
       <nav className="flex justify-between items-center">
-        {/* Vänster länk */}
+        {/* Vänster navigationslänk */}
         <div className="nav-left font-bold">
+          {/* Renderar Portfolio-länken om den finns */}
           {mainLinks.portfolio && (
-            <a
-              key={mainLinks.portfolio.id}
-              href={mainLinks.portfolio.uri}
-              className="link"
-            >
+            <a key={mainLinks.portfolio.id} href={mainLinks.portfolio.uri} className="link">
               {mainLinks.portfolio.title}
             </a>
           )}
         </div>
 
-        {/* Höger länkar */}
+        {/* Höger navigationslänkar */}
         <div className="nav-right flex">
+          {/* Renderar Om Oss- och Kontakta Oss-länkar om de finns */}
           {mainLinks.about && (
-            <a
-              key={mainLinks.about.id}
-              href={mainLinks.about.uri}
-              className="font-bold no-underline text-base ml-2.5"
-            >
+            <a key={mainLinks.about.id} href={mainLinks.about.uri} className="font-bold no-underline text-base ml-2.5">
               {mainLinks.about.title}
             </a>
           )}
           {mainLinks.contact && (
-            <a
-              key={mainLinks.contact.id}
-              href={mainLinks.contact.uri}
-              className="font-bold no-underline text-base ml-2.5"
-            >
+            <a key={mainLinks.contact.id} href={mainLinks.contact.uri} className="font-bold no-underline text-base ml-2.5">
               {mainLinks.contact.title}
             </a>
           )}
         </div>
       </nav>
 
-      {/* Header-sektionen */}
+      {/* Header-sektion */}
       <header className="text-center mt-40">
         <p className="mt-4">{data?.homePage.presentingText}</p>
-        <h1
-          className="text-5xl font-bold"
-          dangerouslySetInnerHTML={{
-            __html: data?.homePage.homePageTitle.replace(
-              "fueled",
-              "fueled<br>"
-            ),
-          }}
-        ></h1>
+        {/* Dynamiskt renderad titel */}
+        <h1 className="text-5xl font-bold" dangerouslySetInnerHTML={{ __html: data?.homePage.homePageTitle.replace("fueled", "fueled<br>") }}></h1>
       </header>
 
-      {/* "Explore Works"-knappen och andra länkar */}
+      {/* Knapp och länkar för utforskning */}
       <div className="text-center mt-7">
-        <a
-          href={data?.homePage.buttonUrl}
-          className="py-2.5 px-6 bg-blue-500 text-white uppercase rounded-full cursor-pointer no-underline text-base transition-colors duration-300 ease inline-block mt-5 mb-40"
-        >
+        <a href={data?.homePage.buttonUrl} className="py-2.5 px-6 bg-blue-500 text-white uppercase rounded-full cursor-pointer no-underline text-base transition-colors duration-300 ease inline-block mt-5 mb-40">
           {data?.homePage.buttonText}
         </a>
       </div>
 
+      {/* Kategorifilter */}
       <FilterCategory categories={categories} />
-      {/* Inläggen */}
-      {/* Posts Container */}
+
+      {/* Inläggskontainer */}
       <div className="grid grid-cols-3 gap-4 mb-16">
-        {filteredPosts.map((post: any) => (
-          <div key={post.id} className="w-full pb-[100%] relative mb-16">
-            <Link href={`/projects/${post.slug}`}>
-              <img
-                src={post.featuredImage.node.mediaItemUrl}
-                alt={post.title}
-                className="absolute w-full h-full object-cover mb-40"
-              />
-              <div className="absolute w-full bottom-0 mb-[-5rem] p-4 bg-white flex flex-col items-center justify-center">
-                <h2 className="text-lg font-bold text-center">{post.title}</h2>
-                <p>{post.PostInfo.subtitle}</p>
-              </div>
-            </Link>
+        {/* Konditionell rendering av inlägg eller ett meddelande om inga inlägg finns */}
+        {hasPosts ? (
+          filteredPosts.map((post: any) => (
+            <div key={post.id} className="w-full pb-[100%] relative mb-16">
+              <Link href={`/projects/${post.slug}`}>
+                <img src={post.featuredImage.node.mediaItemUrl} alt={post.title} className="absolute w-full h-full object-cover mb-40" />
+                <div className="absolute w-full bottom-0 mb-[-5rem] p-4 bg-white flex flex-col items-center justify-center">
+                  <h2 className="text-lg font-bold text-center">{post.title}</h2>
+                  <p>{post.PostInfo.subtitle}</p>
+                </div>
+              </Link>
+            </div>
+          ))
+        ) : (
+          <div className="text-center w-full">
+            <p>Inga inlägg hittades under den valda kategorin.</p>
           </div>
-        ))}
+        )}
       </div>
 
-      <PaginationControls
-        hasNextPage={pageInfo.hasNextPage}
-        hasPrevPage={Number(page) > 1}
-        endCursor={pageInfo.endCursor}
-        startCursor={pageInfo.startCursor}
-        data={undefined}
-        beforeCursor={""}
-        posts={""}
-      />
+      {/* Pagineringskontroller */}
+      <PaginationControls hasNextPage={pageInfo.hasNextPage} hasPrevPage={Number(page) > 1} endCursor={pageInfo.endCursor} startCursor={pageInfo.startCursor} data={undefined} beforeCursor={""} posts={""} />
 
-      <FreelanceSection
-        freelanceTitle={data?.homePage.freelanceProjects.freelanceTitle}
-        freelanceDescription={
-          data?.homePage.freelanceProjects.freelanceDescription
-        }
-        freelanceContactUrl={
-          data?.homePage.freelanceProjects.freelanceContactUrl
-        }
-        freelanceProjectsButton={
-          data?.homePage.freelanceProjects.freelanceProjectsButton
-        }
-      />
+      {/* Frilanssektion */}
+      <FreelanceSection freelanceTitle={data?.homePage.freelanceProjects.freelanceTitle} freelanceDescription={data?.homePage.freelanceProjects.freelanceDescription} freelanceContactUrl={data?.homePage.freelanceProjects.freelanceContactUrl} freelanceProjectsButton={data?.homePage.freelanceProjects.freelanceProjectsButton} />
+
+      {/* Sidfot */}
       <Footer />
     </main>
   );
