9d2256a455e87458240efe2ba6f619c9b30624ad
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-01-11 13:49:33 +0100

    deleted endCursor and startCursor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,15 +15,17 @@ export default async function Home({
   searchParams: { [key: string]: string | string[] | undefined };
 }) {
   // Hantering av sökparametrar för sidnummer och antal poster per sida.
-  const page = Array.isArray(searchParams["page"]) ? searchParams["page"][0] : searchParams["page"] ?? "1";
-  const perPage = Array.isArray(searchParams["per_page"]) ? searchParams["per_page"][0] : searchParams["per_page"] ?? "6";
-
-  // Hantering av sökparametrar för paginering.
-  const endCursor = Array.isArray(searchParams["after"]) ? searchParams["after"][0] : searchParams["after"] ?? "";
-  const beforeCursor = Array.isArray(searchParams["before"]) ? searchParams["before"][0] : searchParams["before"] ?? "";
-
+  const page = Array.isArray(searchParams["page"])
+    ? searchParams["page"][0]
+    : searchParams["page"] ?? "1";
+  const perPage = Array.isArray(searchParams["per_page"])
+    ? searchParams["per_page"][0]
+    : searchParams["per_page"] ?? "6";
+    
   // Hantering av kategori-ID från sökparametrar.
-  const categoryId = Array.isArray(searchParams["categoryId"]) ? searchParams["categoryId"][0] : searchParams["categoryId"];
+  const categoryId = Array.isArray(searchParams["categoryId"])
+    ? searchParams["categoryId"][0]
+    : searchParams["categoryId"];
 
   // Hämtning av inlägg, kategorier och sidinformation från API.
   const { posts, categories, pageInfo } = await getPosts(
@@ -49,9 +51,14 @@ export default async function Home({
   // Filtrering av inlägg baserat på vald kategori.
   let filteredPosts = posts;
   if (categoryId) {
-    const categoryMatch = categories.find((category: { databaseId: any }) => category.databaseId === categoryId);
+    const categoryMatch = categories.find(
+      (category: { databaseId: any }) => category.databaseId === categoryId
+    );
     if (categoryMatch) {
-      filteredPosts = posts.filter((post: { categoryDatabaseId: any }) => post.categoryDatabaseId === categoryId);
+      filteredPosts = posts.filter(
+        (post: { categoryDatabaseId: any }) =>
+          post.categoryDatabaseId === categoryId
+      );
     }
   }
   const hasPosts = filteredPosts.length > 0;
@@ -65,7 +72,11 @@ export default async function Home({
         <div className="nav-left font-bold">
           {/* Renderar Portfolio-länken om den finns */}
           {mainLinks.portfolio && (
-            <a key={mainLinks.portfolio.id} href={mainLinks.portfolio.uri} className="link">
+            <a
+              key={mainLinks.portfolio.id}
+              href={mainLinks.portfolio.uri}
+              className="link"
+            >
               {mainLinks.portfolio.title}
             </a>
           )}
@@ -75,25 +86,36 @@ export default async function Home({
         <div className="nav-right flex">
           {/* Renderar Om Oss- och Kontakta Oss-länkar om de finns */}
           {mainLinks.about && (
-            <a key={mainLinks.about.id} href={mainLinks.about.uri} className="font-bold no-underline text-base ml-2.5">
+            <a
+              key={mainLinks.about.id}
+              href={mainLinks.about.uri}
+              className="font-bold no-underline text-base ml-2.5"
+            >
               {mainLinks.about.title}
             </a>
           )}
           {mainLinks.contact && (
-            <a key={mainLinks.contact.id} href={mainLinks.contact.uri} className="font-bold no-underline text-base ml-2.5">
+            <a
+              key={mainLinks.contact.id}
+              href={mainLinks.contact.uri}
+              className="font-bold no-underline text-base ml-2.5"
+            >
               {mainLinks.contact.title}
             </a>
           )}
         </div>
       </nav>
 
-      <Header 
+      <Header
         titleHtml={data?.homePage.homePageTitle.replace("fueled", "fueled<br>")}
         presentingText={data?.homePage.presentingText}
       />
       {/* Knapp och länkar för utforskning */}
       <div className="text-center mt-7">
-        <a href={data?.homePage.buttonUrl} className="py-2.5 px-6 bg-blue-500 text-white uppercase rounded-full cursor-pointer no-underline text-base transition-colors duration-300 ease inline-block mt-5 mb-40">
+        <a
+          href={data?.homePage.buttonUrl}
+          className="py-2.5 px-6 bg-blue-500 text-white uppercase rounded-full cursor-pointer no-underline text-base transition-colors duration-300 ease inline-block mt-5 mb-40"
+        >
           {data?.homePage.buttonText}
         </a>
       </div>
@@ -108,30 +130,53 @@ export default async function Home({
           filteredPosts.map((post: any) => (
             <div key={post.id} className="w-full pb-[100%] relative mb-16">
               <Link href={`/projects/${post.slug}`}>
-                <img src={post.featuredImage.node.mediaItemUrl} alt={post.title} className="absolute w-full h-full object-cover mb-40" />
+                <img
+                  src={post.featuredImage.node.mediaItemUrl}
+                  alt={post.title}
+                  className="absolute w-full h-full object-cover mb-40"
+                />
                 <div className="absolute w-full bottom-0 mb-[-5rem] p-4 bg-white flex flex-col items-center justify-center">
-                  <h2 className="text-lg font-bold text-center">{post.title}</h2>
+                  <h2 className="text-lg font-bold text-center">
+                    {post.title}
+                  </h2>
                   <p>{post.PostInfo.subtitle}</p>
                 </div>
               </Link>
             </div>
           ))
         ) : (
-          <div className="flex justify-center items-center w-full h-screen text-center">
-          <p className="py-3 px-10 md:py-4 md:px-12 lg:py-5 lg:px-16 text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl text-black font-bold uppercase rounded-full cursor-pointer no-underline transition-colors duration-300 ease inline-block">
-            Inga inlägg hittades under den valda kategorin.
-          </p>
-        </div>
-        
-        
+          <div className="flex justify-center items-center w-full h-screen">
+            <p className="text-md md:text-lg lg:text-xl xl:text-2xl 2xl:text-3xl font-bold uppercase">
+              Inga inlägg hittades under den valda kategorin.
+            </p>
+          </div>
         )}
       </div>
 
       {/* Pagineringskontroller */}
-      <PaginationControls hasNextPage={pageInfo.hasNextPage} hasPrevPage={Number(page) > 1} endCursor={pageInfo.endCursor} startCursor={pageInfo.startCursor} data={undefined} beforeCursor={""} posts={""} />
+      <PaginationControls
+        hasNextPage={pageInfo.hasNextPage}
+        hasPrevPage={Number(page) > 1}
+        endCursor={pageInfo.endCursor}
+        startCursor={pageInfo.startCursor}
+        data={undefined}
+        beforeCursor={""}
+        posts={""}
+      />
 
       {/* Frilanssektion */}
-      <FreelanceSection freelanceTitle={data?.homePage.freelanceProjects.freelanceTitle} freelanceDescription={data?.homePage.freelanceProjects.freelanceDescription} freelanceContactUrl={data?.homePage.freelanceProjects.freelanceContactUrl} freelanceProjectsButton={data?.homePage.freelanceProjects.freelanceProjectsButton} />
+      <FreelanceSection
+        freelanceTitle={data?.homePage.freelanceProjects.freelanceTitle}
+        freelanceDescription={
+          data?.homePage.freelanceProjects.freelanceDescription
+        }
+        freelanceContactUrl={
+          data?.homePage.freelanceProjects.freelanceContactUrl
+        }
+        freelanceProjectsButton={
+          data?.homePage.freelanceProjects.freelanceProjectsButton
+        }
+      />
 
       {/* Sidfot */}
       <Footer />
