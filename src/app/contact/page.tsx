2dd5da811c2c6d1ae6aa2f22602dff1541405f11
Eleonora Nocentini Sköldebrink <112484819+Elli2022@users.noreply.github.com>
2024-01-12 11:09:34 +0100

    almost done with contact site

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 const ContactPage = () => {
   return (
-    <div className="flex min-h-screen bg-gray-900 text-white flex-col justify-center">
+    <div className="min-h-screen bg-gray-900 text-white flex flex-col justify-center">
       <div className="container mx-auto px-4">
         <div className="text-left py-5">
           <Link href="/home" className="text-white font-bold text-l">
@@ -15,7 +15,7 @@ const ContactPage = () => {
         <div className="flex flex-wrap justify-center gap-10">
           <div className="flex-1 max-w-md">
             <div className="text-center mb-10">
-              <h1 className="text-4xl font-bold mb-4">Contact now</h1>
+              <h1 className="text-4xl font-bold mb-4 text-left">Contact now</h1>
               <p>
                 Have a project or question? Send me a message. I will reply within 48 hours.
               </p>
@@ -23,28 +23,28 @@ const ContactPage = () => {
 
             <form className="space-y-6">
               <div>
-                <label htmlFor="name" className="block text-sm font-medium">
+              <label htmlFor="name" className="block text-sm ">
                   Your name
                 </label>
-                <input type="text" id="name" className="w-full mt-1 px-4 py-2 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                <input type="text" id="name" className="w-full bg-transparent border-grey border-b border-gray-600 focus:ring-0 text-white" />
               </div>
               <div>
-                <label htmlFor="email" className="block text-sm font-medium">
+                <label htmlFor="email" className="block text-sm ">
                   Email address
                 </label>
-                <input type="email" id="email" className="w-full mt-1 px-4 py-2 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                <input type="email" id="email" className="w-full bg-transparent border-grey border-b border-gray-600 focus:ring-0 text-white" />
               </div>
               <div>
-                <label htmlFor="subject" className="block text-sm font-medium">
+                <label htmlFor="subject" className="block text-sm ">
                   Subject
                 </label>
-                <input type="text" id="subject" className="w-full mt-1 px-4 py-2 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500" />
+                <input type="text" id="subject" className="w-full bg-transparent border-grey border-b border-gray-600 focus:ring-0 text-white" />
               </div>
               <div>
-                <label htmlFor="message" className="block text-sm font-medium">
+                <label htmlFor="message" className="block text-sm ">
                   Write your message
                 </label>
-                <textarea id="message" rows="4" className="w-full mt-1 px-4 py-2 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-indigo-500"></textarea>
+                <textarea id="message" rows="4" className="w-full bg-transparent border-grey border-b border-gray-600 focus:ring-0 text-white"></textarea>
               </div>
               <div className="flex justify-center">
                 <button type="submit" className="mt-4 px-6 py-2 border border-indigo-600 text-indigo-600 hover:bg-indigo-600 hover:text-white focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50 font-bold uppercase tracking-wide">
